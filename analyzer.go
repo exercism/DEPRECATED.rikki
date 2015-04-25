89b8db0d03e423b2409bc785aab9db7dee0953da
Laura Frank <ljfrank@gmail.com>
2015-04-25 19:23:08 +0200

    WIP
    Signed-off-by: Max Lielje <max@maxlielje.co>

--- analyzer.go
+++ analyzer.go
@@ -6,10 +6,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"strings"
 	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/jrallison/go-workers"
 )
@@ -31,9 +31,9 @@ func NewAnalyzer(exercism, analysseur, auth string) *Analyzer {
 }
 
 type codePayload struct {
-	TrackID string `json:"track_id"`
-	SolutionFiles     map[string]string `json:"solution_files"`
-	Error    string `json:"error"`
+	TrackID       string            `json:"track_id"`
+	SolutionFiles map[string]string `json:"solution_files"`
+	Error         string            `json:"error"`
 }
 
 type analysisResult struct {
@@ -57,6 +57,14 @@ func (analyzer *Analyzer) process(msg *workers.Msg) {
 		lgr.Printf("unable to determine submission key - %s\n", err)
 		return
 	}
+
+	submissionType, err := msg.Args().GetIndex(1).String()
+	if err != nil {
+		lgr.Printf("unable to determine message type - %s\n", err)
+		return
+	}
+
+	// fmt.Println(submissionKey, submissionType)
 
 	// Step 1: get code + language from exercism.io api
 	url := fmt.Sprintf("%s/api/v1/submissions/%s", analyzer.exercismHost, submissionKey)
@@ -97,79 +105,92 @@ func (analyzer *Analyzer) process(msg *workers.Msg) {
 		solutions = append(solutions, solution)
 	}
 
-	// Step 2: submit code to analysseur
-	url = fmt.Sprintf("%s/analyze/%s", analyzer.analysseurHost, cp.TrackID)
-	codeBody := struct {
-		Code string `json:"code"`
-	}{
-		strings.Join(solutions, "\n"),
-	}
-	codeBodyJSON, err := json.Marshal(codeBody)
-	if err != nil {
-		lgr.Printf("%s - %s\n", submissionKey, err)
-		return
-	}
-
-	req, err = http.NewRequest("POST", url, bytes.NewReader(codeBodyJSON))
-	if err != nil {
-		lgr.Printf("%s - cannot prepare request to %s - %s\n", submissionKey, url, err)
-		return
-	}
-	resp, err = http.DefaultClient.Do(req)
-	if err != nil {
-		lgr.Printf("%s - request to %s failed - %s\n", submissionKey, url, err)
-		return
-	}
-
-	body, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		lgr.Printf("%s - failed to fetch submission - %s\n", submissionKey, err)
-		return
-	}
-	resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		lgr.Printf("%s - %s responded with status %d - %s\n", submissionKey, url, resp.StatusCode, string(body))
-		return
-	}
-
-	var ap analysisPayload
-	err = json.Unmarshal(body, &ap)
-	if err != nil {
-		lgr.Printf("%s - %s\n", submissionKey, err)
-		return
-	}
-
-	if ap.Error != "" {
-		lgr.Printf("analysis api is complaining about %s - %s\n", submissionKey, ap.Error)
-		return
-	}
-
-	if len(ap.Results) == 0 {
-		// no feedback, bailing
-		return
-	}
-
-	sanity := log.New(os.Stdout, "SANITY: ", log.Ldate|log.Ltime|log.Lshortfile)
-	for _, result := range ap.Results {
-		for _, key := range result.Keys {
-			sanity.Printf("%s : %s - %s\n", submissionKey, result.Type, key)
-		}
-	}
-
-	// Step 3: Find comments based on analysis result
+	// check if this is a special submission that does not need analysis
 	var comments [][]byte
-	for _, result := range ap.Results {
-		for _, key := range result.Keys {
-			c := NewComment(cp.TrackID, result.Type, key)
-			b, err := c.Bytes()
-			if err != nil {
-				lgr.Printf("We probably need to add a comment at %s - %s\n", c.Path(), err)
+	switch submissionType {
+
+	case "hello_world":
+		fmt.Println("submitting hello world comment")
+
+		comments = append(comments, []byte("Hello World Comment"))
+
+	default:
+		fmt.Println("analyzing code")
+
+		// Step 2: submit code to analysseur
+		url = fmt.Sprintf("%s/analyze/%s", analyzer.analysseurHost, cp.TrackID)
+		codeBody := struct {
+			Code string `json:"code"`
+		}{
+			strings.Join(solutions, "\n"),
+		}
+		codeBodyJSON, err := json.Marshal(codeBody)
+		if err != nil {
+			lgr.Printf("%s - %s\n", submissionKey, err)
+			return
+		}
+
+		req, err = http.NewRequest("POST", url, bytes.NewReader(codeBodyJSON))
+		if err != nil {
+			lgr.Printf("%s - cannot prepare request to %s - %s\n", submissionKey, url, err)
+			return
+		}
+		resp, err = http.DefaultClient.Do(req)
+		if err != nil {
+			lgr.Printf("%s - request to %s failed - %s\n", submissionKey, url, err)
+			return
+		}
+
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			lgr.Printf("%s - failed to fetch submission - %s\n", submissionKey, err)
+			return
+		}
+		resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			lgr.Printf("%s - %s responded with status %d - %s\n", submissionKey, url, resp.StatusCode, string(body))
+			return
+		}
+
+		var ap analysisPayload
+		err = json.Unmarshal(body, &ap)
+		if err != nil {
+			lgr.Printf("%s - %s\n", submissionKey, err)
+			return
+		}
+
+		if ap.Error != "" {
+			lgr.Printf("analysis api is complaining about %s - %s\n", submissionKey, ap.Error)
+			return
+		}
+
+		if len(ap.Results) == 0 {
+			// no feedback, bailing
+			return
+		}
+
+		sanity := log.New(os.Stdout, "SANITY: ", log.Ldate|log.Ltime|log.Lshortfile)
+		for _, result := range ap.Results {
+			for _, key := range result.Keys {
+				sanity.Printf("%s : %s - %s\n", submissionKey, result.Type, key)
 			}
-			if len(b) > 0 {
-				comments = append(comments, b)
+		}
+
+		// Step 3: Find comments based on analysis result
+		for _, result := range ap.Results {
+			for _, key := range result.Keys {
+				c := NewComment(cp.TrackID, result.Type, key)
+				b, err := c.Bytes()
+				if err != nil {
+					lgr.Printf("We probably need to add a comment at %s - %s\n", c.Path(), err)
+				}
+				if len(b) > 0 {
+					comments = append(comments, b)
+				}
 			}
 		}
 	}
+
 	if len(comments) == 0 {
 		// no comments, bailing
 		return
@@ -185,28 +206,30 @@ func (analyzer *Analyzer) process(msg *workers.Msg) {
 	}{
 		s,
 	}
-	commentBodyJSON, err := json.Marshal(commentBody)
-	if err != nil {
-		lgr.Println(err)
-		return
-	}
 
-	url = fmt.Sprintf("%s/api/v1/submissions/%s/comments?shared_key=%s", analyzer.exercismHost, submissionKey, analyzer.auth)
-	req, err = http.NewRequest("POST", url, bytes.NewReader(commentBodyJSON))
-	if err != nil {
-		lgr.Printf("cannot prepare request to %s - %s\n", url, err)
-		return
-	}
-	resp, err = http.DefaultClient.Do(req)
-	if err != nil {
-		lgr.Printf("request to %s failed - %s\n", url, err)
-		return
-	}
+	fmt.Printf("%v", commentBody)
+	// commentBodyJSON, err := json.Marshal(commentBody)
+	// if err != nil {
+	// 	lgr.Println(err)
+	// 	return
+	// }
 
-	body, err = ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if resp.StatusCode != http.StatusNoContent {
-		lgr.Printf("%s responded with status %d - %s\n", url, resp.StatusCode, string(body))
-		return
-	}
+	// url = fmt.Sprintf("%s/api/v1/submissions/%s/comments?shared_key=%s", analyzer.exercismHost, submissionKey, analyzer.auth)
+	// req, err = http.NewRequest("POST", url, bytes.NewReader(commentBodyJSON))
+	// if err != nil {
+	// 	lgr.Printf("cannot prepare request to %s - %s\n", url, err)
+	// 	return
+	// }
+	// resp, err = http.DefaultClient.Do(req)
+	// if err != nil {
+	// 	lgr.Printf("request to %s failed - %s\n", url, err)
+	// 	return
+	// }
+
+	// body, err = ioutil.ReadAll(resp.Body)
+	// resp.Body.Close()
+	// if resp.StatusCode != http.StatusNoContent {
+	// 	lgr.Printf("%s responded with status %d - %s\n", url, resp.StatusCode, string(body))
+	// 	return
+	// }
 }
