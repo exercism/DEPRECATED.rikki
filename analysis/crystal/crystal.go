55772638ad9ff657b59b08118d53081421ee82a7
Max Helmetag <mhelmetag@gmail.com>
2016-10-17 22:34:56 -0700

    fixup request content-type for json, minor var name changes

--- analysis/crystal/crystal.go
+++ analysis/crystal/crystal.go
@@ -13,7 +13,13 @@ import (
 // Host is the base URL for the analysseur API.
 var Host string
 
-type result struct {
+type request struct {
+	ID       string `json:"id"`
+	Contents string `json:"contents"`
+}
+
+type response struct {
+	ID       string    `json:"id"`
 	Problems []problem `json:"problems"`
 	Error    string    `json:"error"`
 }
@@ -23,7 +29,7 @@ type problem struct {
 	Result string `json:"result"`
 }
 
-// Analyze Crystal code for formatting errors
+// Analyze Crystal code for formatting errors (and other bad things maybe later)
 func Analyze(files map[string]string) ([]string, error) {
 	var sources []string
 	for _, source := range files {
@@ -31,36 +37,35 @@ func Analyze(files map[string]string) ([]string, error) {
 	}
 
 	url := fmt.Sprintf("%s/check", Host)
-	codeBody := struct {
-		Code string `json:"code"`
-	}{
-		strings.Join(sources, "\n"),
-	}
-	codeBodyJSON, err := json.Marshal(codeBody)
+	code := strings.Join(sources, "\n")
+	requestBody := request{ID: "rikki", Contents: code}
+	requestBodyJSON, err := json.Marshal(requestBody)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest("POST", url, bytes.NewReader(codeBodyJSON))
+	req, err := http.NewRequest("POST", url, bytes.NewReader(requestBodyJSON))
 	if err != nil {
 		return nil, err
 	}
+
+	req.Header.Set("Content-Type", "application/json")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 	resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("%s responded with status %d - %s\n", url, resp.StatusCode, string(body))
+		return nil, fmt.Errorf("%s responded with status %d - %s\n", url, resp.StatusCode, string(respBody))
 	}
 
-	var res result
-	err = json.Unmarshal(body, &res)
+	var res response
+	err = json.Unmarshal(respBody, &res)
 	if err != nil {
 		return nil, err
 	}
